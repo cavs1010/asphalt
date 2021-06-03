c6ff8c88d56e45768cc748d44d42b9bda3604369
Camilo <cavs1010@gmail.com>
2021-06-03 17:52:39 +1000

    init

--- main.py
+++ main.py
@@ -6,9 +6,4 @@ import numpy as np
 df = pd.read_excel('fileML.xlsx', sheet_name = 'AirVoids', engine='openpyxl')
 df = df.set_index('Air Voids ID')
 df = df.applymap(str)
-#df.columns = df.columns.str.strip()
 df = df.applymap(str.strip)
-
-
-#%% 
-
