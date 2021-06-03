9ba852f7ac68c90409ec7811212d1ee9ca00bc95
Camilo <cavs1010@gmail.com>
2021-06-03 14:17:20 +1000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+#%% IMPORTS
 import pandas as pd
+import numpy as np
+
+#%% Data Reading and initial pre-processing
+df = pd.read_excel('fileML.xlsx', sheet_name = 'AirVoids', engine='openpyxl')
+df = df.set_index('Air Voids ID')
+df = df.applymap(str)
+#df.columns = df.columns.str.strip()
+df = df.applymap(str.strip)
+
+
+#%% 
+
